bd5e49ebe6881591e319bc069c590a0257f74f94
arkadiy16 <arkadiy160702@gmail.com>
2024-03-25 00:25:21 +0300

    Created new files. conftest.py - settings for tests requirements.txt - requirements for venv test_main_page.py - first testing case

---
+++ test_main_page.py
@@ -0,0 +1,15 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.common.exceptions import TimeoutException
+
+
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    browser.get(link)
+    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
+    login_link.click()
